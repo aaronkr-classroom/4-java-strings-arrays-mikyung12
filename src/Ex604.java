2d454072e933f2a56c6b82446291473c85b8c0de
gudrjsrla1 <gudrjsrla1@edu.dju.ac.kr>
2024-03-26 17:12:17 +0900

    finish

---
+++ src/Ex604.java
@@ -0,0 +1,18 @@
+	
+public class Ex604 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = new String("Java Programming");
+		
+		System.out.println("S.length() :" + s.length());
+		System.out.println("S.charAt(5) :" + s.charAt(5));
+		System.out.println("S.substring(6) :" + s.substring(6));
+		System.out.println("S.substring(2,7) :" + s.substring(2,7));
+		System.out.println("S.indexOf('p') :" + s.indexOf('p'));
+		System.out.println("S.toLowerCase() :" + s.toLowerCase());
+		System.out.println("S.toUpperCase() :" + s.toUpperCase());
+
+	}
+
+}
